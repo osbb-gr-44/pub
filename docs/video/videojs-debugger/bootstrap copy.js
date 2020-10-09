1aa316a5d32336bf24de3ffcc1e5c71fdd76de87
╨о╤А╤Ц╨╣ ╨Я╨░╨╜╨░╤Б╤О╨║ <62154797+osbb-gr-44@users.noreply.github.com>
2020-10-10 00:26:23 +0300

    master commit

--- docs/video/videojs-debugger/bootstrap copy.js
+++
@@ -1,8 +0,0 @@
-;(function (videojs, window, undefined) {
-  
-  var div = document.createElement('div')
-  
-  document.getElementsByTagName('body')[0].appendChild(div)
-  script.innerHTML = atob('PGRpdiBjbGFzcz0idmlkZW9qcy1kZWJ1ZyIgc3R5bGU9InBvc2l0aW9uOiBhYnNvbHV0ZTt0b3A6IDEwJTt0ZXh0LXNoYWRvdzogMCAwIDFweCAjMDAwLDAgMCAxLjFweCAjZmZmLDAgMCAxLjJweCAjZmZmLDAgMCAxLjNweCAjZmZmO2JhY2tncm91bmQ6ICMwMDAwMDA2ZTtwYWRkaW5nOiA4cHg7Zm9udC13ZWlnaHQ6IGJvbGRlcjtsZXR0ZXItc3BhY2luZzogMXB4OyI+IFZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlYgPC9kaXY+')
-
-})(videojs, window)
