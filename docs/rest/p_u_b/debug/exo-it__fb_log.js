ff7869b5eddb01cf3b75d1fd7fc21147ac394c9e
╨о╤А╤Ц╨╣ ╨Я╨░╨╜╨░╤Б╤О╨║ <62154797+osbb-gr-44@users.noreply.github.com>
2020-10-11 13:08:03 +0300

    master commit

--- docs/rest/p_u_b/debug/exo-it__fb_log.js
+++ docs/rest/p_u_b/debug/exo-it__fb_log.js
@@ -58,6 +58,8 @@ window.initFirebaseDb = function (_ctr) {
     })
     var database = firebase.database()
     _ctr.__fbDb = {}
+    _ctr.__fbDb.db = database
+    _ctr.__fbDb._rf= _ctr.idx+'/'+_d.id
     _ctr.__fbDb.send = _d => messagesRef.ref(_d.id).set(_d)
     // Reference to the /messages/ database path.
     var messagesRef = database.ref(_ctr.idx)
