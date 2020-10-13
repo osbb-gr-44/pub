fd2e251367a0defc87fb22bc7cd0313f9a369ad5
╨о╤А╤Ц╨╣ ╨Я╨░╨╜╨░╤Б╤О╨║ <62154797+osbb-gr-44@users.noreply.github.com>
2020-10-13 16:38:14 +0300

    master commit

--- docs/rest/p_u_b/debug/exo-it__fb_log.example.js
+++ docs/rest/p_u_b/debug/exo-it__fb_log.example.js
@@ -11,17 +11,16 @@ __log({
 })
 
 __log({
-    id: Date.now(),
-    t: 'Budni Osbb Z Ihorem Michudoyu Hist Olifer Serhiy',
-    p: 'http://127.0.0.1:8080/2.jpg',
-    m3u8: 'http://127.0.0.1:8080/v.m3u8'
-  })
+  id: Date.now(),
+  t: 'Budni Osbb Z Ihorem Michudoyu Hist Olifer Serhiy',
+  p: 'http://127.0.0.1:8080/2.jpg',
+  m3u8: 'http://127.0.0.1:8080/v.m3u8'
+})
 _ctr.__fbDb.send({ idx: 'video-js:dbg', d: { t: 1.745577, v: 0.2 } })
 
-
-_ctr.opt.__fbDb.val().then(val=>console.log(val))
+_ctr.opt.__fbDb.val().then(val => console.log(val))
 
 _ctr.opt.__fbDb.del()
 
-__log.__fbCon.fbDbReceive=console.info
-__log({'adasd':672})
+__log.__fbCon.opt.receive = console.log
+__log('log', { adasd: 672 })
