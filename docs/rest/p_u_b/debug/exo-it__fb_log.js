a5a85ce560020f901105237e2dc2bf5e35e34f2a
╨о╤А╤Ц╨╣ ╨Я╨░╨╜╨░╤Б╤О╨║ <62154797+osbb-gr-44@users.noreply.github.com>
2020-10-17 00:29:10 +0300

    master commit

--- docs/rest/p_u_b/debug/exo-it__fb_log.js
+++ docs/rest/p_u_b/debug/exo-it__fb_log.js
@@ -1,115 +1,172 @@
-var addScriptNext = (src_url, cb) => {
-  if (cb()) {
-    return Promise.resolve()
-  } else {
-    return new Promise((resolve, reject) => {
-      ;((src_url, cb) => {
-        var _opt = 0
-
-        if (typeof src_url === 'object') {
-          _opt = src_url
-          src_url = _opt.url
-        }
-
-        if (
-          ![].slice
-            .call(document.querySelectorAll('script[src]'))
-            .find(_ => _.getAttribute('src') === src_url)
-        ) {
-          var script = document.createElement('script')
-
-          if (_opt && _opt.attr) {
-            Object.keys(_opt.attr).map(_ => {
-              script.setAttribute(_, _opt.attr[_])
-            })
-          }
-          script.src = src_url
-          script.type = 'text/javascript' // no need for HTML5
-          cb(script)
-          document.getElementsByTagName('body')[0].appendChild(script) // for IE6
-        }
-      })(src_url, function (script) {
-        function _next () {
-          setTimeout(function () {
-            if (!cb()) {
-              _next()
-            } else {
-              resolve(script)
-            }
-          }, 300)
-        }
-        _next()
-      })
-    })
+var a0_0xc199 = [
+  'remove',
+  'exo-it.firebaseapp.com',
+  'undefined',
+  'AIzaSyA7XK_xLY-EPZ5nwcHsVpdaZwIgNyiZBMQ',
+  'map',
+  'call',
+  'exports',
+  'appendChild',
+  'bind',
+  'url',
+  'off',
+  'default',
+  'text/javascript',
+  'constructor',
+  'create',
+  'body',
+  'receive',
+  'del',
+  'symbol',
+  'function',
+  'database',
+  'set',
+  'https://www.gstatic.com/firebasejs/3.6.9/firebase.js',
+  'src',
+  'on\x20setMessage',
+  'value',
+  'type',
+  'Module',
+  'iterator',
+  'createElement',
+  'object',
+  'hasOwnProperty',
+  'child_added',
+  'querySelectorAll',
+  'prototype',
+  './src/exo-it__fb_log.js',
+  'orderByChild',
+  'toStringTag',
+  'exo-it.appspot.com',
+  'getElementsByTagName',
+  'child_changed',
+  'val',
+  'limitToLast',
+  'idx',
+  'slice',
+  'defineProperty',
+  'ref',
+  '__esModule',
+  'attr',
+  'script[src]',
+  '498521469323',
+  'orderRef',
+  '__fbDb',
+  'resolve',
+  'initFirebaseDb',
+  'key'
+]
+;(function (_0x40cb7b, _0xc199bd) {
+  var _0x5c645e = function (_0x2254cf) {
+    while (--_0x2254cf) {
+      _0x40cb7b['push'](_0x40cb7b['shift']())
+    }
   }
+  _0x5c645e(++_0xc199bd)
+})(a0_0xc199, 0x1bb)
+var a0_0x5c64 = function (_0x40cb7b, _0xc199bd) {
+  _0x40cb7b = _0x40cb7b - 0x0
+  var _0x5c645e = a0_0xc199[_0x40cb7b]
+  return _0x5c645e
 }
-
-window.initFirebaseDb = function (_ctr) {
-  addScriptNext(
-    'https://www.gstatic.com/firebasejs/3.6.9/firebase.js',
-    () => window.firebase
-  ).then(_script => {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyA7XK_xLY-EPZ5nwcHsVpdaZwIgNyiZBMQ',
-      authDomain: 'exo-it.firebaseapp.com',
-      databaseURL: 'https://exo-it.firebaseio.com',
-      storageBucket: 'exo-it.appspot.com',
-      messagingSenderId: '498521469323'
+!(function (_0x2254cf) {
+  var _0x406a20 = a0_0x5c64,
+    _0x2e3c04 = {}
+  function _0x330b23 (_0x5226bf) {
+    var _0x3d1ce3 = a0_0x5c64
+    if (_0x2e3c04[_0x5226bf]) return _0x2e3c04[_0x5226bf][_0x3d1ce3('0xb')]
+    var _0x49a3ef = (_0x2e3c04[_0x5226bf] = {
+      i: _0x5226bf,
+      l: !0x1,
+      exports: {}
     })
-    var database = firebase.database()
-    _ctr.__fbDb = {}
-    _ctr.__fbDb.db = database
-    _ctr.__fbDb.del = () =>
-      firebase
-        .database()
-        .ref(_ctr.idx)
-        .remove()
-
-    _ctr.__fbDb.val = () =>
-      firebase
-        .database()
-        .ref(_ctr.idx)
-        .once('value')
-        .then(snapshot => snapshot.val())
-
-    _ctr.__fbDb.send = _d =>
-      firebase
-        .database()
-        .ref(_ctr.idx + '/' + _d.id)
-        .set(_d)
-    // Reference to the /messages/ database path.
-
-    var messagesRef = _ctr.orderRef
-      ? database.ref(_ctr.idx).orderByChild(_ctr.orderRef)
-      : database.ref(_ctr.idx)
-
-    // Make sure we remove all previous listeners.
-    // messagesRef.off()
-
-    
-    var receiveFn = 0
-
-    Object.defineProperty(_ctr.__fbDb, 'receive', {
-      set: function (v) {
-        messagesRef.off()
-        if (v) {
-          receiveFn = v
-          var setMessage = function (data) {
-            if(receiveFn&& typeof receiveFn==="function")
-            {
-              receiveFn('on setMessage', data.key, data.val())
-            }
-          }
-          messagesRef.limitToLast(20).on('child_added', setMessage)
-          messagesRef.limitToLast(20).on('child_changed', setMessage)
-        } else {
-          receiveFn = 0
-        }
-      },
-      get: function () {
-        return receiveFn
-      }
-    })
-  })
-  return _ctr
-}
+    return (
+      _0x2254cf[_0x5226bf]['call'](
+        _0x49a3ef['exports'],
+        _0x49a3ef,
+        _0x49a3ef[_0x3d1ce3('0xb')],
+        _0x330b23
+      ),
+      (_0x49a3ef['l'] = !0x0),
+      _0x49a3ef[_0x3d1ce3('0xb')]
+    )
+  }
+  ;(_0x330b23['m'] = _0x2254cf),
+    (_0x330b23['c'] = _0x2e3c04),
+    (_0x330b23['d'] = function (_0x57139b, _0x37d977, _0x49dd60) {
+      var _0x54b496 = a0_0x5c64
+      _0x330b23['o'](_0x57139b, _0x37d977) ||
+        Object[_0x54b496('0x32')](_0x57139b, _0x37d977, {
+          enumerable: !0x0,
+          get: _0x49dd60
+        })
+    }),
+    (_0x330b23['r'] = function (_0x29b29b) {
+      var _0x445a14 = a0_0x5c64
+      _0x445a14('0x7') != typeof Symbol &&
+        Symbol[_0x445a14('0x2a')] &&
+        Object['defineProperty'](_0x29b29b, Symbol[_0x445a14('0x2a')], {
+          value: _0x445a14('0x20')
+        }),
+        Object[_0x445a14('0x32')](_0x29b29b, _0x445a14('0x34'), { value: !0x0 })
+    }),
+    (_0x330b23['t'] = function (_0x3f5cba, _0x30b710) {
+      var _0x5268d1 = a0_0x5c64
+      if (
+        (0x1 & _0x30b710 && (_0x3f5cba = _0x330b23(_0x3f5cba)), 0x8 & _0x30b710)
+      )
+        return _0x3f5cba
+      if (
+        0x4 & _0x30b710 &&
+        _0x5268d1('0x23') == typeof _0x3f5cba &&
+        _0x3f5cba &&
+        _0x3f5cba[_0x5268d1('0x34')]
+      )
+        return _0x3f5cba
+      var _0x1e58b5 = Object[_0x5268d1('0x13')](null)
+      if (
+        (_0x330b23['r'](_0x1e58b5),
+        Object['defineProperty'](_0x1e58b5, _0x5268d1('0x10'), {
+          enumerable: !0x0,
+          value: _0x3f5cba
+        }),
+        0x2 & _0x30b710 && 'string' != typeof _0x3f5cba)
+      )
+        for (var _0x225856 in _0x3f5cba)
+          _0x330b23['d'](
+            _0x1e58b5,
+            _0x225856,
+            function (_0x1d05e1) {
+              return _0x3f5cba[_0x1d05e1]
+            }[_0x5268d1('0xd')](null, _0x225856)
+          )
+      return _0x1e58b5
+    }),
+    (_0x330b23['n'] = function (_0x287beb) {
+      var _0x1d2664 = a0_0x5c64,
+        _0x33fe90 =
+          _0x287beb && _0x287beb[_0x1d2664('0x34')]
+            ? function () {
+                return _0x287beb['default']
+              }
+            : function () {
+                return _0x287beb
+              }
+      return _0x330b23['d'](_0x33fe90, 'a', _0x33fe90), _0x33fe90
+    }),
+    (_0x330b23['o'] = function (_0x11a612, _0x22fd34) {
+      var _0x30c643 = a0_0x5c64
+      return Object[_0x30c643('0x27')][_0x30c643('0x24')][_0x30c643('0xa')](
+        _0x11a612,
+        _0x22fd34
+      )
+    }),
+    (_0x330b23['p'] = ''),
+    _0x330b23((_0x330b23['s'] = _0x406a20('0x28')))
+})({
+  './src/exo-it__fb_log.js': function (_0x5b8382, _0x5d013a) {
+    eval(
+      "var _0x1377b9 = a0_0x5c64;function _0x15e48d(_0xf1ce75) {\n    var _0x1199c3 = a0_0x5c64;\n    '@babel/helpers\\\\x20-\\\\x20typeof';\n    return typeof Symbol === _0x1199c3('0x18') && typeof Symbol[_0x1199c3('0x21')] === _0x1199c3('0x17') ? _0x15e48d = function _0xb6d972(_0x48866c) {\n        return typeof _0x48866c;\n    } : _0x15e48d = function _0x36d84e(_0x5c6313) {\n        var _0x41437a = _0x1199c3;\n        return _0x5c6313 && typeof Symbol === _0x41437a('0x18') && _0x5c6313[_0x41437a('0x12')] === Symbol && _0x5c6313 !== Symbol[_0x41437a('0x27')] ? _0x41437a('0x17') : typeof _0x5c6313;\n    }, _0x15e48d(_0xf1ce75);\n}var _0x18edc8 = function _0x4266dc(_0x2429ac, _0x33a984) {\n    var _0x379a34 = a0_0x5c64;\n    return _0x33a984() ? Promise[_0x379a34('0x2')]() : new Promise(function (_0x3366cc, _0x5cf984) {\n        ;\n        (function (_0x3b42a1, _0x95267) {\n            var _0x2b1165 = a0_0x5c64, _0x1866af = 0;\n            _0x15e48d(_0x3b42a1) === _0x2b1165('0x23') && (_0x1866af = _0x3b42a1, _0x3b42a1 = _0x1866af[_0x2b1165('0xe')]);\n            if (![][_0x2b1165('0x31')][_0x2b1165('0xa')](document[_0x2b1165('0x26')](_0x2b1165('0x36')))['find'](function (_0x3fe5b7) {\n                    var _0x458145 = _0x2b1165;\n                    return _0x3fe5b7['getAttribute'](_0x458145('0x1c')) === _0x3b42a1;\n                })) {\n                var _0x307aea = document[_0x2b1165('0x22')]('script');\n                _0x1866af && _0x1866af[_0x2b1165('0x35')] && Object['keys'](_0x1866af[_0x2b1165('0x35')])[_0x2b1165('0x9')](function (_0x1ce155) {\n                    var _0x535e1a = _0x2b1165;\n                    _0x307aea['setAttribute'](_0x1ce155, _0x1866af[_0x535e1a('0x35')][_0x1ce155]);\n                }), _0x307aea[_0x2b1165('0x1c')] = _0x3b42a1, _0x307aea[_0x2b1165('0x1f')] = _0x2b1165('0x11'), _0x95267(_0x307aea), document[_0x2b1165('0x2c')](_0x2b1165('0x14'))[0][_0x2b1165('0xc')](_0x307aea);\n            }\n        }(_0x2429ac, function (_0x591c18) {\n            function _0x5a299b() {\n                setTimeout(function () {\n                    !_0x33a984() ? _0x5a299b() : _0x3366cc(_0x591c18);\n                }, 300);\n            }\n            _0x5a299b();\n        }));\n    });\n};window[_0x1377b9('0x3')] = function (_0x2d583c) {\n    var _0x3a2c81 = _0x1377b9;\n    return _0x18edc8(_0x3a2c81('0x1b'), function () {\n        return window['firebase'];\n    })['then'](function (_0x597735) {\n        var _0x17bc4c = _0x3a2c81;\n        firebase['initializeApp']({\n            'apiKey': _0x17bc4c('0x8'),\n            'authDomain': _0x17bc4c('0x6'),\n            'databaseURL': 'https://exo-it.firebaseio.com',\n            'storageBucket': _0x17bc4c('0x2b'),\n            'messagingSenderId': _0x17bc4c('0x37')\n        });\n        var _0x100dbb = firebase[_0x17bc4c('0x19')]();\n        _0x2d583c['__fbDb'] = {}, _0x2d583c[_0x17bc4c('0x1')]['db'] = _0x100dbb, _0x2d583c[_0x17bc4c('0x1')][_0x17bc4c('0x16')] = function () {\n            var _0x1f400d = _0x17bc4c;\n            return firebase[_0x1f400d('0x19')]()[_0x1f400d('0x33')](_0x2d583c[_0x1f400d('0x30')])[_0x1f400d('0x5')]();\n        }, _0x2d583c[_0x17bc4c('0x1')][_0x17bc4c('0x2e')] = function () {\n            var _0x5a5af6 = _0x17bc4c;\n            return firebase[_0x5a5af6('0x19')]()[_0x5a5af6('0x33')](_0x2d583c['idx'])['once'](_0x5a5af6('0x1e'))['then'](function (_0x5512d4) {\n                var _0x23df09 = _0x5a5af6;\n                return _0x5512d4[_0x23df09('0x2e')]();\n            });\n        }, _0x2d583c[_0x17bc4c('0x1')]['send'] = function (_0x25553d) {\n            var _0x448143 = _0x17bc4c;\n            return firebase[_0x448143('0x19')]()['ref'](_0x2d583c[_0x448143('0x30')] + '/' + _0x25553d['id'])[_0x448143('0x1a')](_0x25553d);\n        };\n        var _0x172a5a = _0x2d583c['orderRef'] ? _0x100dbb[_0x17bc4c('0x33')](_0x2d583c['idx'])[_0x17bc4c('0x29')](_0x2d583c[_0x17bc4c('0x0')]) : _0x100dbb[_0x17bc4c('0x33')](_0x2d583c[_0x17bc4c('0x30')]), _0x1cca1d = 0;\n        Object['defineProperty'](_0x2d583c[_0x17bc4c('0x1')], _0x17bc4c('0x15'), {\n            'set': function _0x96beea(_0x2f32b2) {\n                var _0x395dd5 = _0x17bc4c;\n                _0x172a5a[_0x395dd5('0xf')]();\n                if (_0x2f32b2) {\n                    _0x1cca1d = _0x2f32b2;\n                    var _0x4f4ae2 = function _0x4d9eb0(_0x3d8103) {\n                        var _0x4629e0 = _0x395dd5;\n                        _0x1cca1d && typeof _0x1cca1d === _0x4629e0('0x18') && _0x1cca1d(_0x4629e0('0x1d'), _0x3d8103[_0x4629e0('0x4')], _0x3d8103[_0x4629e0('0x2e')]());\n                    };\n                    _0x172a5a[_0x395dd5('0x2f')](20)['on'](_0x395dd5('0x25'), _0x4f4ae2), _0x172a5a[_0x395dd5('0x2f')](20)['on'](_0x395dd5('0x2d'), _0x4f4ae2);\n                } else\n                    _0x1cca1d = 0;\n            },\n            'get': function _0x1b16f0() {\n                return _0x1cca1d;\n            }\n        });\n    }), _0x2d583c;\n};"
+    )
+  }
+})
