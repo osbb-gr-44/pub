608508b2f7951513cd7857f965082256b2d5be11
Юрій Панасюк <62154797+osbb-gr-44@users.noreply.github.com>
2022-02-06 10:48:23 +0200

    master ✨ commit

---
+++ docs/video/videojs-debugger/bootstrap.js
@@ -0,0 +1,8 @@
+;(function (videojs, window, undefined) {
+  
+  var div = document.createElement('div')
+  
+  document.getElementsByTagName('body')[0].appendChild(div)
+  div.innerHTML = atob('PGRpdiBjbGFzcz0idmlkZW9qcy1kZWJ1ZyIgc3R5bGU9InBvc2l0aW9uOiBhYnNvbHV0ZTt0b3A6IDEwJTt0ZXh0LXNoYWRvdzogMCAwIDFweCAjMDAwLDAgMCAxLjFweCAjZmZmLDAgMCAxLjJweCAjZmZmLDAgMCAxLjNweCAjZmZmO2JhY2tncm91bmQ6ICMwMDAwMDA2ZTtwYWRkaW5nOiA4cHg7Zm9udC13ZWlnaHQ6IGJvbGRlcjtsZXR0ZXItc3BhY2luZzogMXB4OyI+PC9kaXY+')
+
+})(videojs, window)
